7e50e66a3ce9b38d15fcea16d3ed61a4bfd75756
zhuiyisi <34240569+zhuiyisi@users.noreply.github.com>
2023-10-06 23:20:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var timestamp = Date.parse(new Date());
+time_Start = new Date('2023-5-24 21:30:00').getTime()
+var time = (timestamp - time_Start)/1000;
+
+function timedCount()
+{
+document.getElementById('shijian').innerHTML = fmoney(time,0);
+time = time + 1;
+t = setTimeout("timedCount()",1000);
+}
+
+/** 每三位数字添加一个逗号，方便阅读 **/
+
+function fmoney(s, n)   
+{   
+   n = n > 0 && n <= 20 ? n : 2;   
+   s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";   
+   var l = s.split(".")[0].split("").reverse(),   
+   r = s.split(".")[1];   
+   t = "";   
+   for(i = 0; i < l.length; i ++ )   
+   {   
+      t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");   
+   }   
+   return t.split("").reverse().join("") /*+ "." + r*/;   
+}
